d8012cf7b2a9adcce027582a43cc57f264aa2b10
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-30 17:05:05 +0100

    Create home page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+    def Home
+    end
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
  resources :recipes 
  resources :foods 
 
+ get 'home', to: 'static#home'
+
+
   # Defines the root path route ("/")
 
    root "recipes#index"
